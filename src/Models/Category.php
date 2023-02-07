3caad3428324fe0ab2e386a971de54105d18d75d
shibomb <shibomb@users.noreply.github.com>
2023-02-07 05:36:28 +0000

    Fix styling

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -2,7 +2,6 @@
 
 namespace Shibomb\FilamentTodo\Models;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;

dfb8732e043f5bbd173b0872fda13f4359c5ca4c
SHIBAHARA Hiroki <shibomb@gmail.com>
2023-10-04 15:47:17 +0900

    Update :  improve Category Actions layout

--- src/Resources/CategoryResource.php
+++ src/Resources/CategoryResource.php
@@ -80,9 +80,9 @@ class CategoryResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\ViewAction::make(),
+                // Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                // Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- src/Resources/CategoryResource/Pages/EditCategory.php
+++ src/Resources/CategoryResource/Pages/EditCategory.php
@@ -2,10 +2,18 @@
 
 namespace Shibomb\FilamentTodo\Resources\CategoryResource\Pages;
 
+use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Shibomb\FilamentTodo\Resources\CategoryResource;
 
 class EditCategory extends EditRecord
 {
     protected static string $resource = CategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
 }
